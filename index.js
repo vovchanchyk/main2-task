3ff45203b4b13cf9bedaea0b511fb6ffce1368b6
Vladimyr Grabovsky <oberigfast@gmail.com>
2020-07-22 17:30:43 +0300

    add mobile

---
+++ index.js
@@ -0,0 +1,8 @@
+
+
+function showMenu(){
+    document.querySelector('.mobile-menu--hidden').classList.toggle('mobile-menu--show');
+    document.querySelector('.mobile-btn').classList.toggle('mobile-btn--active');
+    
+}
+
