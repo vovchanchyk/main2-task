5df9b21e7abc8e72a0717adf50bf31ae5edde0e4
Vovchanchyk <oberigfast@gmail.com>
2020-09-13 13:10:18 +0300

    13.09.20

--- index.js
+++ index.js
@@ -29,26 +29,42 @@ if (isMobile.any()) {
   body.addClass("touch");
   let arrowMenu = $(".arrow");
   arrowMenu.on("click", function () {
+    let parent = $(this).closest(".menu");
+    parent.toggleClass("parent");
     $(this).toggleClass("arrow-rotate");
     $(this).next().toggleClass("open-submenu");
+    if (parent.find(".open-submenu").length > 0) {
+      parent.find(".open-submenu").removeClass("open-submenu");
+      parent.find(".arrow-rotate").removeClass("arrow-rotate");
+      $(this).toggleClass("arrow-rotate");
+      $(this).next().toggleClass("open-submenu");
+    }
+    $(window).resize(function () {
+      if ($(window).width() > 768) {
+        arrowMenu.removeClass("arrow-rotate");
+        arrowMenu.next().removeClass("open-submenu");
+        $(".header__menu-block").removeClass("header__menu-block--open");
+        $(".burger__img").removeClass("close");
+      }
+    });
   });
 } else {
   body.addClass("mouse");
   let arrowMenu = $(".arrow");
   arrowMenu.on("click", function () {
-    if($(window).width()< 768){
-    $(this).toggleClass("arrow-rotate");
-    $(this).next().toggleClass("open-submenu");
+    if ($(window).width() < 768) {
+      $(this).toggleClass("arrow-rotate");
+      $(this).next().toggleClass("open-submenu");
     }
   });
   $(window).resize(function () {
-    if($(window).width()>768){
-    arrowMenu.removeClass("arrow-rotate")
-    arrowMenu.next().removeClass("open-submenu")
+    if ($(window).width() > 768) {
+      arrowMenu.removeClass("arrow-rotate");
+      arrowMenu.next().removeClass("open-submenu");
+      $(".header__menu-block").removeClass("header__menu-block--open");
+      $(".burger__img").removeClass("close");
     }
-    
-  })
-
+  });
 }
 
 let selected = function () {
@@ -56,6 +72,7 @@ let selected = function () {
   let selectItem = $(".select__item");
   function openSelect(params) {
     $(this).parent().toggleClass("is-active");
+    
   }
 
   function selectChoose() {
@@ -71,6 +88,7 @@ let selected = function () {
 };
 
 selected();
+
 $(document).on("click", function (event) {
   if (
     $(".select").is(event.target) != true &&
@@ -78,7 +96,18 @@ $(document).on("click", function (event) {
   ) {
     $(".select").removeClass("is-active");
   }
+
+  if ($(".header__menu").hasClass("parent")) {
+    if($('header__menu').is(event.target) != true &&
+     $(".header__menu").has(event.target).length === 0
+    ){
+      $(".open-submenu").removeClass("open-submenu");
+      $(".arrow-rotate").removeClass("arrow-rotate");
+    }
+  }
 });
+
+
 
 let searchF = $(".search__val");
 
@@ -115,21 +144,22 @@ function showPopup() {
   $(".popup").removeClass("close");
 }
 function closePopup() {
-  $(".popup").addClass("close");
+  $(".popup").addClass("close"); //-----
 }
 $("#close").on("click", closePopup);
 
 $(".popup").on("click", function (event) {
+  //-- можно впихнуть тру
   if (
     $(".login").is(event.target) != true &&
     $(".login").has(event.target).length === 0 &&
     $(event.target).is("#btn-login") != true
   ) {
-     closePopup();
+    closePopup();
   }
 });
 
-let eye = $('.showPass')
+let eye = $(".showPass");
 
 function showPass() {
   let input = $("#password");
@@ -137,77 +167,73 @@ function showPass() {
     input.attr("type", "text");
     eye.toggleClass("fa-eye");
     eye.toggleClass("fa-eye-slash");
-  }else{
+  } else {
     input.attr("type", "password");
     eye.toggleClass("fa-eye");
     eye.toggleClass("fa-eye-slash");
   }
 }
-eye.on('click', function () {
-    showPass();
-})
+eye.on("click", function () {
+  showPass();
+});
 
-$('#password').on('input', function () {
-    if($(this).val()!='' &&( $(this).val().length < 5 || $(this).val().length > 9 )){
-        $(this).parent().addClass('incorrect')
-    }else{
-        $(this).parent().removeClass('incorrect')  
-    }
-})
-
+$("#password").on("input", function () {
+  if (
+    $(this).val() != "" &&
+    ($(this).val().length < 5 || $(this).val().length > 9)
+  ) {
+    $(this).parent().addClass("incorrect");
+  } else {
+    $(this).parent().removeClass("incorrect");
+  }
+});
 
 function validPass() {
-    let valPass = $('#password').val();
-    let reg = /[A-Za-z1-9]{5,9}/;
-    if(valPass.length <= 9 && reg.test(valPass)){
-        return true;
-    }    
+  let valPass = $("#password").val();
+  let reg = /[A-Za-z1-9]{5,9}/;
+  if (valPass.length <= 9 && reg.test(valPass)) {
+    return true;
+  }
 }
 
 function validEmail() {
-    let valEmail = $('#email').val();
-    let re = /\S+@\S+\.\S+/;
-    if( valEmail != "" && re.test(valEmail)){
-        return true
-    }else{
-        alert('enter valid email')
-        
-    }
-    
+  let valEmail = $("#email").val();
+  let re = /\S+@\S+\.\S+/;
+  if (valEmail != "" && re.test(valEmail)) {
+    return true;
+  } else {
+    alert("enter valid email");
+  }
 }
 
 function validName() {
-    let valName = $('#name').val();
-    if(valName != ''){
-        return true
-    }
-    
+  let valName = $("#name").val();
+  if (valName != "") {
+    return true;
+  }
 }
-
 
 function correct() {
-    if(validName() && validPass() && validEmail() != true){
-        $('#email').val('')
-        $('#email').attr('placeholder','enter the input')    
-    }else if(validPass() != true && validEmail() && validName()){
-        $('#password').val('')
-        $('#password').attr('placeholder','enter the input')
-    }else if(validPass() && validEmail() && validName() != true){
-        $('#name').val('')
-        $('#name').attr('placeholder','enter the input')
-    }else if(validPass() && validEmail() && validName()){
-        alert('thank you')
-        $('.login__input').val('')
-        closePopup()
-    }else{
-        $('.login__input').val('')
-        $('.login__input').attr('placeholder','enter the input')
-    }
-    
+  if (validName() && validPass() && validEmail() != true) {
+    $("#email").val("");
+    $("#email").attr("placeholder", "enter the input");
+  } else if (validPass() != true && validEmail() && validName()) {
+    $("#password").val("");
+    $("#password").attr("placeholder", "enter the input");
+  } else if (validPass() && validEmail() && validName() != true) {
+    $("#name").val("");
+    $("#name").attr("placeholder", "enter the input");
+  } else if (validPass() && validEmail() && validName()) {
+    alert("thank you");
+    $(".login__input").val("");
+    closePopup();
+  } else {
+    $(".login__input").val("");
+    $(".login__input").attr("placeholder", "enter the input");
+  }
 }
 
-$('.login__btn').on('click', function () {
-    correct()
-    console.log(1)
-    
-})
+$(".login__btn").on("click", function () {
+  correct();
+  console.log(1);
+});
